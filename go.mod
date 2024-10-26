ac0272b9fc7e757b4d9127c603a90104c937009d
RomBurunduk <47496016+RomBurunduk@users.noreply.github.com>
2024-10-26 15:22:14 +0300

    add articles (#2)
    Co-authored-by: davidsonrd <davidsonrd@mts.ru>

--- go.mod
+++ go.mod
@@ -14,6 +14,11 @@ require (
 )
 
 require (
+	github.com/JohannesKaufmann/html-to-markdown v1.6.0 // indirect
+	github.com/PuerkitoBio/goquery v1.10.0 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/go-co-op/gocron v1.37.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -25,12 +30,15 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- internal/scrapping/service.go
+++ internal/scrapping/service.go
@@ -2,13 +2,21 @@ package scrapping
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/go-co-op/gocron"
 	"net/http"
 	"scrapping_service/internal/database"
 	"scrapping_service/internal/scrapping/migrations"
+	"scrapping_service/internal/scrapping/models"
 	"scrapping_service/internal/scrapping/repository"
 	"scrapping_service/pkg/middlewares"
 	"scrapping_service/pkg/utils"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -28,14 +36,23 @@ type Service struct {
 	conf  *Conf
 
 	server *http.Server
+	client *http.Client
 
 	repo *repository.Repository
 
+	cron *gocron.Scheduler
+
 	// поля для скрапинга
+	converter *md.Converter
 }
 
 func NewService(ctx context.Context, name, namespace string) *Service {
-	return &Service{ctx: ctx, Conv: utils.NewConv(name, namespace)}
+	return &Service{
+		ctx:       ctx,
+		Conv:      utils.NewConv(name, namespace),
+		cron:      gocron.NewScheduler(time.UTC),
+		converter: md.NewConverter("", true, nil),
+	}
 }
 
 func (s *Service) Configure(conf *Conf, confDb *database.Conf) {
@@ -53,10 +70,36 @@ func (s *Service) Configure(conf *Conf, confDb *database.Conf) {
 
 		s.repo = repository.NewRepository(db.DBX)
 
+		s.client = &http.Client{CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			if strings.Contains(req.URL.String(), "articles") {
+				return nil
+			}
+			return errors.New("redirected")
+		}}
+
 		s.RunWorker(s.start, "start", 1)
+
+		_, err := s.cron.Every(time.Minute * 30).Do(s.scrap)
+		if err != nil {
+			log.Error().Str("module", s.Name).Msgf("start cron for scrapping: %v", err)
+		}
+
+		s.cron.StartAsync()
 	})
 
 	log.Info().Str("module", s.Name).Msg("conf: configure end")
+}
+
+func (s *Service) setConf(conf *Conf) {
+	s.xConf.Lock()
+	s.conf = conf
+	s.xConf.Unlock()
+}
+
+func (s *Service) getConf() *Conf {
+	s.xConf.RLock()
+	defer s.xConf.RUnlock()
+	return s.conf
 }
 
 func (s *Service) start() {
@@ -96,20 +139,163 @@ func (s *Service) start() {
 	}
 }
 
-func (s *Service) setConf(conf *Conf) {
-	s.xConf.Lock()
-	s.conf = conf
-	s.xConf.Unlock()
+func (s *Service) scrap() {
+	lastArticleSite, err := getLastArticle()
+	if err != nil {
+		log.Error().Str("module", s.Name).Msgf("GetLastArticle from site error: %v", err)
+		return
+	}
+
+	lastArticle, err := s.repo.GetLastArticle(s.ctx)
+	if err != nil {
+		if repository.IsNotFoundError(err) {
+			lastArticle = lastArticleSite - 30
+		} else {
+			log.Error().Str("module", s.Name).Msgf("GetLastArticle from repo error: %v", err)
+			return
+		}
+	}
+	if lastArticleSite < lastArticle {
+		log.Warn().Str("module", s.Name).Msgf("strange article ids: in repo %v, in site %v", lastArticle, lastArticleSite)
+		return
+	}
+
+	for i := lastArticle + 1; i <= lastArticleSite; i++ {
+		// выходим из цикла
+		select {
+		case <-s.ctx.Done():
+			return
+		default:
+		}
+
+		article, err := s.getArticle(i)
+		if err != nil {
+			log.Error().Str("module", s.Name).Msgf("getArticle error: %v", err)
+			continue
+		}
+		repoArticle, err := mapArticle(article)
+		if err != nil {
+			log.Error().Str("module", s.Name).Msgf("mapArticle error: %v", err)
+			continue
+		}
+		err = s.repo.AddArticle(s.ctx, repoArticle)
+		if err != nil {
+			log.Error().Str("module", s.Name).Msgf("error in repo: %v", err)
+			continue
+		}
+	}
 }
 
-func (s *Service) getConf() *Conf {
-	s.xConf.RLock()
-	defer s.xConf.RUnlock()
-	return s.conf
+func getLastArticle() (int64, error) {
+	doc, err := goquery.NewDocument("https://habr.com/ru/articles/")
+	if err != nil {
+		return 0, err
+	}
+
+	var articleID int64
+	var parseErr error
+	found := false
+
+	doc.Find("article[class=tm-articles-list__item]").First().Each(func(i int, item *goquery.Selection) {
+		val, ok := item.Attr("id")
+		if !ok {
+			parseErr = errors.New("attribute 'id' not found")
+			return
+		}
+
+		id, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			parseErr = err
+			return
+		}
+
+		articleID = id
+		found = true
+	})
+
+	if parseErr != nil {
+		return 0, parseErr
+	}
+
+	if !found {
+		return 0, errors.New("no article found")
+	}
+
+	return articleID, nil
+}
+
+func (s *Service) getArticle(id int64) (*models.Article, error) {
+	url := fmt.Sprintf("https://habr.com/ru/articles/%v/", id)
+	get, err := s.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if get.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("not received 200 status code: %v", get.StatusCode)
+	}
+	defer get.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(get.Body)
+	if err != nil {
+		return nil, err
+	}
+	var (
+		name, text, complexity string
+		readingTime            int64
+		tags                   []string
+	)
+	doc.Find("div[xmlns='http://www.w3.org/1999/xhtml']").Each(func(i int, item *goquery.Selection) {
+		text = s.converter.Convert(item)
+	})
+
+	doc.Find("h1[class='tm-title tm-title_h1']").Each(func(i int, item *goquery.Selection) {
+		name = item.Text()
+	})
+
+	doc.Find("span[class=tm-article-complexity__label]").Each(func(i int, item *goquery.Selection) {
+		complexity = item.Text()
+	})
+
+	doc.Find("span[class=tm-article-reading-time__label]").Each(func(i int, item *goquery.Selection) {
+		arr := strings.Split(item.Text(), " ")
+		readingTime, _ = strconv.ParseInt(arr[0], 10, 64)
+	})
+
+	doc.Find("a[class=tm-tags-list__link]").Each(func(i int, item *goquery.Selection) {
+		tag := item.Text()
+		if tag != "" {
+			tags = append(tags, tag)
+		}
+	})
+
+	return &models.Article{
+		Id:          id,
+		Name:        name,
+		Text:        text,
+		Complexity:  complexity,
+		ReadingTime: readingTime,
+		Tags:        tags,
+	}, nil
+}
+
+func mapArticle(article *models.Article) (*repository.Article, error) {
+	tags, err := json.Marshal(article.Tags)
+	if err != nil {
+		return nil, err
+	}
+	return &repository.Article{
+		Id:          article.Id,
+		Name:        article.Name,
+		Text:        article.Text,
+		Complexity:  article.Complexity,
+		ReadingTime: article.ReadingTime,
+		Tags:        tags,
+	}, nil
 }
 
 func (s *Service) WaitTerminate() {
 	log.Info().Str("module", s.Name).Msg("scrapper server term: begin")
+
+	s.cron.Stop()
 
 	ctx, cancel := context.WithTimeout(s.ctx, time.Second*10)
 
